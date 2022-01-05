f360028b1ab523c5277ebf03471b49deb115c86f
DESKTOP-GKB7E59\chjung <heeyame@gmail.com>
2022-01-05 21:16:27 +0900

    도커

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting simple-service "
+echo "********************************************************"
+java -jar /usr/local/restful-web-service/restful-web-service-0.0.1-SNAPSHOT.jar
